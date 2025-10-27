9cb6c4a2afa3c9c61e4ad23938d0dcd779b19261
Kieu Tho <shine190704@gmail.com>
2025-10-27 11:32:49 +0700

    dientichhcn

--- Source.cpp
+++ Source.cpp
@@ -7,10 +7,14 @@ return a + b;
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int dtHCN(int a, int b){
+return a*b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout <<"Tong:"<< tong(10,5);
+	cout <<"dien tich hcn:"<< dtHCN(10,10);
 
 	system("pause");
 	return 0;
