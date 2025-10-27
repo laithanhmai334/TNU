6c0e4fa702894f0a228a1ad2a021e2672044d736
Truong Nha Nguyen <truongnhanguyen0312@gmail.com>
2025-10-27 10:59:18 +0700

    Tich

--- Source.cpp
+++ Source.cpp
@@ -7,13 +7,16 @@ return a + b;
 int hieu(int a, int b){
 	return a-b;
 }
-int tich(int a, int b);
+int tich(int a, int b) {
+	return a*b;
+}
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout <<"Tong:"<< tong(10,5);
 	cout <<"Hieu:"<< hieu(10,5);
+	cout << "Tich: " >> tich(10,5);
 	system("pause");
 	return 0;
 }
